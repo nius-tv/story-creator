3e97635b2971fde2420af0c831abb852616d3da2
Carlos Chinchilla <calufa@gmail.com>
2020-01-26 10:34:04 -0500

    update MIN_NUM_IMAGES

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 BUCKET_NAME = 'plasmic-stories'
 COMPUTE_PROJECT_NAME = 'plasmic-compute-256214'
-MIN_NUM_IMAGES = 5
+MIN_NUM_IMAGES = 2
 PIX_TO_PIX_MODEL = 'nanaimo-bar'
 REQUIRED_FIELDS = ['GCSImageURLs', 'subtitle', 'text', 'title1', 'title2']
 TOPIC_NAME = 'init-generate'
